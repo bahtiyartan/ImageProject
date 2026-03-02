51a338e39ea333cfc8b33fb8caa2c675c919a44f
FidanAkyurek <fdn-akyurek@hotmail.com>
2026-03-02 11:29:50 +0300

    Operation

--- src/com/ias/image/processing/logic/operations/ImageOperation.java
+++
@@ -1,8 +0,0 @@
-package com.ias.image.processing.logic.operations;
-
-import java.awt.image.BufferedImage;
-
-public interface ImageOperation {
-        BufferedImage apply(BufferedImage input);
-        String getOperationName();
-}
